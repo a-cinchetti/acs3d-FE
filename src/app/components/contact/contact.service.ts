177091155d8d221d3d819c9dabdafc5eac7ce52c
cin <andrea.cinchetti@intre.it>
2024-03-31 20:50:24 +0200

    add headers

--- src/app/components/contact/contact.service.ts
+++ src/app/components/contact/contact.service.ts
@@ -11,7 +11,13 @@ export class ContactService {
   }
 
   sendEmailRequest(data: any) {
-    this.http.post('https://acs3d-emailsender.acworker.workers.dev/api/sendmail', data)
+    this.http.post('https://acs3d-emailsender.acworker.workers.dev/api/sendmail', data, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+
+    })
       .subscribe((res) => {
         console.log(res);
       });
